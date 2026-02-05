5176ad1e34a52e12c267b9934cf0365751bc76fd
Andrew S <andrews54757@gmail.com>
2026-02-05 15:15:03 -0500

    update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,11 +5,8 @@ import { siteConfig } from "@/lib/siteConfig";
 export function Footer() {
   return (
     <footer className="border-t py-6 text-center text-xs text-gray-500 dark:text-gray-400">
-      Built for the {siteConfig.siteName} archive. See the code on{" "}
-      <a href="https://github.com/Storage-Tech-2/Storage-Tech-2.github.io" className="underline">
-        Github
-      </a>
-      .
+      Built for the {siteConfig.siteName}.
+      <a href="https://llamamc.org/" className="underline">Join the Llama Collective</a> to contribute or support development.
     </footer>
   );
 }
